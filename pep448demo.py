8ad1ae4d189e6812c716675cb26fa0cbdebaac77
Eric Dennison <ericd@netdenizen.com>
2019-09-28 09:35:25 -0400

    Updated from Brython Server: 9/28/2019 9:35:24 AM

--- pep448demo.py
+++ pep448demo.py
@@ -2,3 +2,7 @@ date_info = {'year': "2020", 'month': "01", 'day': "01"}
 track_info = {'artist': "Beethoven", 'title': 'Symphony No 5'}
 all_info = {**date_info, **track_info}
 print(all_info)
+
+test = {"up": "down"}
+temp = {**test, "hey": "nah"}
+print(temp)
